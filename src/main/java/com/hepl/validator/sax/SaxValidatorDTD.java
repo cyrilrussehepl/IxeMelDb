7e3d066822323f93ce5e82e2a39c1ee0183d1db3
Cyril Russe <cyril.russe@student.hepl.be>
2023-11-01 10:41:47 +0100

    Error handler +/-

--- src/main/java/com/hepl/validator/sax/SaxValidatorDTD.java
+++ src/main/java/com/hepl/validator/sax/SaxValidatorDTD.java
@@ -6,22 +6,46 @@ import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
 import com.hepl.validator.Utils;
-import org.xml.sax.SAXException;
+import org.xml.sax.*;
 
 public class SaxValidatorDTD {
     public static void main(String[] args) {
         SAXParserFactory factory = SAXParserFactory.newInstance();
         factory.setValidating(true);
         factory.setNamespaceAware(false);
+
         try {
             SAXParser sp = factory.newSAXParser();
-            sp.getXMLReader().parse(Utils.XML_FILE);
+            XMLReader reader = getXmlReaderWithErrorHandler(sp);
+            reader.parse(Utils.XML_FILE);
         } catch (ParserConfigurationException e) {
             System.out.println("ParserConfigurationException : " + e.getMessage());
         } catch (IOException e) {
             System.out.println("IOException : " + e.getMessage());
         } catch (SAXException e) {
-            System.out.println("SaxEXception : " + e.getMessage());
+            return;
         }
+        System.out.println("Validation successful!");
+    }
+
+    private static XMLReader getXmlReaderWithErrorHandler(SAXParser sp) throws SAXException {
+        XMLReader reader = sp.getXMLReader();
+
+        reader.setErrorHandler(new ErrorHandler() {
+            public void warning(SAXParseException e){
+                System.out.println("WARNING : " + e.getMessage());
+            }
+
+            public void error(SAXParseException e) throws SAXException {
+                System.out.println("ERROR : " + e.getMessage());
+                throw e;
+            }
+
+            public void fatalError(SAXParseException e) throws SAXException {
+                System.out.println("FATAL : " + e.getMessage());
+                throw e;
+            }
+        });
+        return reader;
     }
 }
